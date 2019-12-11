cc577f229cbed1ff66a8e95653fe6e3399ccea26
xPerdev <perkrol@gmail.com>
2019-12-11 17:23:14 +0100

    Resolved issue with making changes to terraform/aws/userdata.tf

--- terraform/aws/userdata.tf
+++ terraform/aws/userdata.tf
@@ -3,7 +3,7 @@
 ###########################################
 
 data "template_file" "ATMFraudDetection_instance" {
-  template = file("utils/instance.sh")
+  template = file("../utils/bootstrap_aws_amzn_linux_2.sh")
 
   vars = {
     atmfrauddetectiondemo = var.atmfrauddetectiondemo
